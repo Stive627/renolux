58e462bc3122ed0a35262429828fcc769ffe8548
Stive627 <fossistive627@gmail.com>
2025-04-25 18:57:10 +0530

    i am going for gym

--- components/Accueil/Footer.js
+++ components/Accueil/Footer.js
@@ -18,9 +18,9 @@ function Footer() {
             <div className={`flex flex-col w-96 gap-2 justify-center ${!large && 'mt-5'}`}>
                 <p className=' text-slate-900 text-center'>Rejoignez notre groupe WhatsApp pour rester informé de nos dernières offres et découvrir une large gamme de services pour tous vos travaux de rénovation </p>
                 <div className=' flex justify-center'>
-                    <button className='  cursor-pointer w-1/2 bg-black text-white rounded-lg py-0.5 subscribe relative'>
+                    <button className='  cursor-pointer w-1/2 bg-black text-white rounded-lg py-0.5  relative'>
                         <p>Rejoindre</p>
-                        <div className=' w-5 h-5 bg-green-700 absolute -top-2 rounded-full  -right-1 animate-bounce'></div>
+                        <div className=' w-5 h-5  absolute -top-2 rounded-full  -right-1 animate-bounce subscribe'></div>
                     </button>
                 </div>
             </div>
