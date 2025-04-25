93c0df8b94e4b708167e51649741295618fe6fd8
Stive627 <fossistive627@gmail.com>
2025-04-25 21:51:28 +0530

    second commit

--- components/Accueil/Navbar.js
+++ components/Accueil/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 import useScreen from '../../hook/useScreen';
 import CloseIcon from '@mui/icons-material/Close';
@@ -6,6 +6,7 @@ import '../../App.css'
 import fetchLink from '@/functions/fetchLink';
 import Image from 'next/image';
 import handleNavigate from '@/functions/handleNavigate';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 function highlightNav(elt){
     if(typeof window !== 'undefined'){
@@ -60,15 +61,23 @@ const RightLarge = () => {
 
 function Navbar() { 
     const large = useScreen()
+    const contactRef = useRef()
     const [visible, setVisible] = useState(0)
     const handleVisible = () => {
         const vis = Math.floor((window.scrollY/(document.body.clientHeight - window.innerHeight))*100)
+        if(vis>0){ 
+            contactRef.current.style.height = '50px'
+            }
+        else{
+            contactRef.current.style.height = '0px'
+        }
         setVisible(vis)
     }
     useEffect(()=>{
+        
         window.addEventListener('scroll', handleVisible)
         return () => window.removeEventListener('scroll', handleVisible)
-    }, [])
+    }, [visible])
   return (
         <div className=' fixed w-screen bg-white z-40 top-0'>
             <div className = 'flex justify-between px-2 py-2'>
@@ -76,6 +85,18 @@ function Navbar() {
                 { large? <RightLarge/>:<RightSmall/>}
             </div>
             {visible>0 && <hr style={{width:`${visible}%`}} className=' text-blue-400'/>}
+            <div ref={contactRef} className='  border border-gray-200 w-screen contact  '>
+                <div className='py-1 px-2 w-full h-full flex items-center justify-between'>
+                    <div className=' flex flex-row items-center gap-3'>
+                        <PhoneIcon/>
+                        <p>+237 6 91 09 80 37</p>
+                    </div>
+                    <button className='  cursor-pointer w-1/2 bg-black text-white rounded-lg py-0.5  relative'>
+                        <p>Rejoindre</p>
+                        <div className=' w-5 h-5  absolute -top-2 rounded-full  -right-1  subscribe'></div>
+                    </button>
+                </div>
+            </div>
         </div>
   )
 }
